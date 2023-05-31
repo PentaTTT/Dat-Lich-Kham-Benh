aa170fdd5602905e0c9a795860764476d2a38961
PentaTTT <duytan0220@gmail.com>
2023-05-31 07:49:48 +0700

    update assets

--- Frontend/src/containers/HomePage/Section/HandBook.js
+++ Frontend/src/containers/HomePage/Section/HandBook.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Slider from "react-slick";
-import specialtyImg from "../../../assets/handBook/handbook-1.png"
+import handbook from "../../../assets/handBook/handbook.jpg"
+import handbook2 from "../../../assets/handBook/handbook2.jpg"
+import handbook3 from "../../../assets/handBook/handbook3.jpg"
+import handbook4 from "../../../assets/handBook/handbook4.jpg"
 
 class HandBook extends Component {
 
@@ -21,35 +24,35 @@ class HandBook extends Component {
                         <div className='section-body'>
                             <Slider {...this.props.settings}>
                                 <div className=' specialty-customize'>
-                                    <img src={specialtyImg} />
-                                    <h4>Cẩm nang 1</h4>
+                                    <img src={handbook} />
+                                    <h4>Trẻ bị ho về đêm</h4>
                                 </div>
                                 <div className=' specialty-customize'>
-                                    <img src={specialtyImg} />
-                                    <h4>Cẩm nang 2</h4>
+                                    <img src={handbook2} />
+                                    <h4>Cách trị ho và nôn cho trẻ</h4>
                                 </div>
                                 <div className=' specialty-customize'>
-                                    <img src={specialtyImg} />
-                                    <h4>Cẩm nang 3</h4>
+                                    <img src={handbook3} />
+                                    <h4>Khám tai mũi họng tại bệnh viện</h4>
                                 </div>
                                 <div className=' specialty-customize'>
-                                    <img src={specialtyImg} />
-                                    <h4>Cẩm nang 4</h4>
+                                    <img src={handbook4} />
+                                    <h4>Top 9 bác sĩ tại Hà Nội</h4>
                                 </div>
                                 <div className=' specialty-customize'>
-                                    <img src={specialtyImg} />
+                                    <img src={handbook4} />
                                     <h4>Cẩm nang 5</h4>
                                 </div>
                                 <div className=' specialty-customize'>
-                                    <img src={specialtyImg} />
+                                    <img src={handbook4} />
                                     <h4>Cẩm nang 6</h4>
                                 </div>
                                 <div className=' specialty-customize'>
-                                    <img src={specialtyImg} />
+                                    <img src={handbook4} />
                                     <h4>Cẩm nang</h4>
                                 </div>
                                 <div className=' specialty-customize'>
-                                    <img src={specialtyImg} />
+                                    <img src={handbook4} />
                                     <h4>Cẩm nang</h4>
                                 </div>
                             </Slider>
